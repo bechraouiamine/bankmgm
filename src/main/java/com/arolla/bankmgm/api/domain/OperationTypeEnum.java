aa6b4d8c5d6f9fd10fcce6188019f76c2593695c
bechraoui amine <bechraoui.amine@gmail.com>
2021-01-05 11:58:19 +0100

    Update operation enum

--- src/main/java/com/arolla/bankmgm/api/domain/OperationTypeEnum.java
+++ src/main/java/com/arolla/bankmgm/api/domain/OperationTypeEnum.java
@@ -4,5 +4,5 @@ package com.arolla.bankmgm.api.domain;
  * Created by aminebechraoui, on 04/01/2021, in com.arolla.bankmgm.service.domain
  */
 public enum OperationTypeEnum {
-    DEBIT, CREDIT
+    WITHDRAWAL, DEPOSIT
 }
