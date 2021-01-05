a329d5a61f6d99fbcc6e635f78b7639769117d8b
bechraoui amine <bechraoui.amine@gmail.com>
2021-01-05 12:07:10 +0100

    Adding find by name and last name to clientRepository

--- src/main/java/com/arolla/bankmgm/api/repository/ClientRepository.java
+++ src/main/java/com/arolla/bankmgm/api/repository/ClientRepository.java
@@ -9,4 +9,6 @@ import java.util.UUID;
  * Created by aminebechraoui, on 04/01/2021, in com.arolla.bankmgm.service.repository
  */
 public interface ClientRepository extends JpaRepository<Client, UUID> {
+
+    Client findByNameAndLastName(String name, String lastName);
 }
